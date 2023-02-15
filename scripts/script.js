d56e1bde1aeac8f6792362680e6e552c4759879f
sathish <2000sathishkumar.d@gmail.com>
2023-02-15 20:34:08 +0530

    add javascript

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// Get elements values
+const yourEl = document.getElementById("You");
+const crushEl = document.getElementById("crush");
+
+const listEl = document.getElementById("list");
+
+// button elements
+const flamesBtnEl = document.getElementById("flames-button");
+const resetBtnEl = document.getElementById("reset");
+
+// global variable
+
+// init function
+
+// event listeners
